a38e5f9d4107d785d20a34ae2e808da9b126b316
markableidinger <mark.ableidinger@gmail.com>
2014-08-15 10:04:09 -0700

    Stops cycling on mouseover, restarts on mouseoff

--- main.js
+++ main.js
@@ -1,16 +1,35 @@
 
 var contentBox = document.getElementById('content-box')
 var i=0
+var started
 function cycle(){
-if (i<2){
-  i+=1
+  if (i<2){
+    i+=1
+  }
+  else{
+    i=0
+  }
+    contentBox.innerHTML=generator_blank(titles[i], descriptions[i])
 }
-else{
-  i=0
+function start(){
+  started = window.setInterval(function(){cycle()}, 4000)
 }
-  contentBox.innerHTML=contents[i]
-}
-window.setInterval(function(){cycle()}, 4000)
 
-contents=['<div class="content__image"></div><div class="content__description"><h1>Movie Title1</h1><p>Movie Description askjhfkajdhflkSDHFLKJASDHGLKHASDLKGHA D;LDSHFLKJADHF LDKJFHJD FLKAJDG KLAJSDGKLADSG FLKADG FLKJASDGFJKLASDG FLKAJDSG LKAGFKASGD FLHASDKJHF ASDKJFG AKSDJGF AKDJS </p></div>', '<div class="content__image"></div><div class="content__description"><h1>Movie Title2</h1><p>Movie Description ANOTHER DIFFERENT MOIVE DESCRIPTION ARGHHHBARBALEAJSHFKSJAHFOASIUFOIG ADSKHFDSAKF ASDKMF ADS FMSDA FMAKD FKAJSDGAOIRGWNE JD FMSDA KM  --COMING SOOOOOOON--</p></div>','<div class="content__image"></div><div class="content__description"><h1>Event Name</h1><p>WIN A CHANCE TO BE AN EXTRA EATEN BY AN ALIEN IN OUR NEW FILM "EATEN BY ALIENS!"</p> </div>']
+function stop(){
+  window.clearInterval(started)
+}
 
+var titles=['Movie Title1', 'Movie Title2', 'Event Name']
+var descriptions = ['Movie Description askjhfkajdhflkSDHFLKJASDHGLKHASDLKGHA D;LDSHFLKJADHF LDKJFHJD FLKAJDG KLAJSDGKLADSG FLKADG FLKJASDGFJKLASDG FLKAJDSG LKAGFKASGD FLHASDKJHF ASDKJFG AKSDJGF AKDJS', 'DIFFERENT MOVIE ASHLDJASHFLJHDFKJHDSAKF', 'WIN A CHANCE TO BE AN EXTRA EATEN BY AN ALIEN IN OUR NEW FILM "EATEN BY ALIENS!"']
+
+function generator_blank(title, content){
+  return '<div class="content__image"></div><div class="content__description"><h1>'+title+'</h1><p>'+content+'</p></div>'}
+
+function generator(img, title, content){
+  return '<img src="'+img+'"/></div><div class="content__description"><h1>'+title+'</h1><p>'+content+'</p></div>'}
+
+start()
+
+contentBox.addEventListener('mouseover', function(){stop();}, false);
+
+contentBox.addEventListener('mouseout', function(){start();}, false);
