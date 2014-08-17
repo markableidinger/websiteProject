eef6080d30ec88be6852751e1521af9c02be50ce
Nathan <mugatu9@gmail.com>
2014-08-16 17:06:34 -0700

    Trying to create an event listener that will output a different movie description depending on which movie is selected. currently not working.

--- search.js
+++ search.js
@@ -1,16 +1,8 @@
+
+
 var movies = new Array(3);
 var genres = new Array(3);
 var descriptions = new Array(movies.length);
-
-movies[0] = "Psycho Killer";
-movies[1] = "The Army of Midgets";
-movies[2] = "Nightmare on Boren Avenue";
-movies[3] = "Snakes on the Enterprise";
-
-genres[0] = "Science Fiction";
-genres[1] = "Horror";
-genres[2] = "Thriller";
-genres[3] = "Midgets";
 
 var select = document.getElementById("genre");
 var options = ["Horror", "Slasher", "Science Fiction", "Despair", "Monsters", "Psychological", "Zombies", "Demonology"];
@@ -23,8 +15,10 @@ for (i = 0; i < options.length; i++){
   select.appendChild(el);
 }
 
+
+
 select = document.getElementById("title");
-options = ["Psycho Killer", "The Army of Midgets", "Nightmare on Boren Avenue", "Snakes on the Enterprise", "Titanic vs Tsunami", "The Man-Eating Shirt", "The Haunted Stall", ]
+options = ["Psycho Killer", "The Army of Midgets", "Nightmare on Boren Avenue", "Snakes on the Enterprise", "Titanic vs Tsunami", "The Man-Eating Shirt", "The Demon of the Stall", ]
 for (i = 0; i < options.length; i++){
   var opt = options[i];
   var el = document.createElement("option");
@@ -32,3 +26,16 @@ for (i = 0; i < options.length; i++){
   el.value = opt;
   select.appendChild(el);
 }
+
+select.addEventListener('change', function(){changeMovie(options[i])}, false)
+var movieDescriptions = new Array(options.length);
+var movieDescriptions = ["This describes Psycho Killer", "This describes The Army of Midgets", "This describes Nightmare on Boren Avenue", "This describes Snakes on the Enterprise", "This describes Titanic cs Tsunami", "This describes The Man-Eating Shirt", "This describes The Demon of the Stall"]
+var movieImages = new Array(options.length);
+var movieImages = ["Image1", "Image2", "Image3", "Image4", "Image5", "Image6", "Image7", "Image8"]
+
+// function changeMovie(options[i]) {
+//   if (select.options[i].selected) {
+//     var el = document.createElement("p");
+//     el.textContent = movieDescriptions[i].textContent;
+//   }
+// }
