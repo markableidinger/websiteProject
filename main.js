30c3017f581942a82293e5774eb19a3b42f430a3
markableidinger <mark.ableidinger@gmail.com>
2014-08-17 19:27:07 -0700

    add images, content, and finalize search function.

--- main.js
+++ main.js
@@ -1,32 +1,41 @@
 
 var contentBox = document.getElementById('content-box')
-var i=0
+var i=1
 var started
 function cycle(){
   if (i<2){
+    contentBox.innerHTML=generator_subheading(images[i], titles[i], descriptions[i], subheads[i])
     i+=1
   }
   else{
+    contentBox.innerHTML=generator(images[i], titles[i], descriptions[i])
     i=0
   }
-    contentBox.innerHTML=generator_blank(titles[i], descriptions[i])
 }
 function start(){
-  started = window.setInterval(function(){cycle()}, 4000)
+  started = window.setInterval(function(){cycle()}, 5000)
+  contentBox.style.opacity=1
 }
 
 function stop(){
   window.clearInterval(started)
+  contentBox.style.opacity=.8
 }
 
-var titles=['Movie Title1', 'Movie Title2', 'Event Name']
-var descriptions = ['Movie Description askjhfkajdhflkSDHFLKJASDHGLKHASDLKGHA D;LDSHFLKJADHF LDKJFHJD FLKAJDG KLAJSDGKLADSG FLKADG FLKJASDGFJKLASDG FLKAJDSG LKAGFKASGD FLHASDKJHF ASDKJFG AKSDJGF AKDJS', 'DIFFERENT MOVIE ASHLDJASHFLJHDFKJHDSAKF', 'WIN A CHANCE TO BE AN EXTRA EATEN BY AN ALIEN IN OUR NEW FILM "EATEN BY ALIENS!"']
-
-function generator_blank(title, content){
-  return '<div class="content__image"></div><div class="content__description"><h1>'+title+'</h1><p>'+content+'</p></div>'}
+var titles = ['Under the Sea IN SPACE',
+  'Eaten By Aliens',
+  'SWEEPSTAKES!']
+var descriptions = ['In our latest thrilling film, we follow the journey of a group of brave astronauts as they go under the frozen waters of Jupiter\'s moon Europa. However, when their equipment begins to malfunction, they realize that Europa is actually an alien supercomputer, frozen to keep it from destroying the universe... and they just woke it up.',
+  'Jim Smelosky is an average guy just trying to get by, but he loses his job at the local gas station when everyone in town gets eaten by aliens. His rent is due, and he needs to make money fast. He and 3 friends try to make a youtube sensation about the aliens that have been eating people, but get more than they bargained for...',
+  'WIN A CHANCE TO BE AN EXTRA EATEN BY AN ALIEN IN OUR NEW FILM "EATEN BY ALIENS!"']
+var subheads = ['NEW RELEASE', 'COMING SOON']
+var images = ['images/spacePlanet.jpeg', 'images/alien.jpg', 'images/alien.jpg']
 
 function generator(img, title, content){
-  return '<img src="'+img+'" class="content__image"/></div><div class="content__description"><h1>'+title+'</h1><p>'+content+'</p></div>'}
+  return '<img src="'+img+'" class="content__image"/></div><div class="content__description"><h1 class=\'content__description__title\'>'+title+'</h1><p class=\'content__description__body\'>'+content+'</p></div>'}
+
+function generator_subheading(img, title, content, subhead){
+  return '<img src="'+img+'" class="content__image"/></div><div class="content__description"><h1 class=\'content__description__title\'>'+title+'</h1><h2 class=\'content__description__header\'>'+subhead+'</h2><p class=\'content__description__body\'>'+content+'</p></div>'}
 
 start()
 
--- search.js
+++ search.js
@@ -3,19 +3,49 @@ genreBox=document.getElementById('genre__selector')
 movieBox=document.getElementById('movie__selector')
 contentBox=document.getElementById('contentBox')
 //movie genres, titles, and descriptions
-// var genreTitles=["Psycho Midgets","Nightmare Snakes","Mystery Genre","Demons"] Doesn't work
+var genreTitles=["Psycho Midgets","Nightmare Snakes","Nautical","Demons"]
 
-var genreTitles=[1, 2, 3, 4]
-var titles=["Psycho Killer", "The Army of Midgets", "Nightmare on Boren Avenue", "Snakes on the Enterprise", "Titanic vs Tsunami", "The Man-Eating Shirt", "The Demon of the Stall"]
-var descriptions=["This describes Psycho Killer", "This describes The Army of Midgets", "This describes Nightmare on Boren Avenue", "This describes Snakes on the Enterprise", "This describes Titanic cs Tsunami", "This describes The Man-Eating Shirt", "This describes The Demon of the Stall"]
-var movieGenres=[1,1,2,2,3,3,4,4,5,5]
+var titles=["Psycho Killer",
+  "The Army of Midgets",
+  "Nightmare on Boren Avenue",
+  "Snakes on the Enterprise",
+  "Titanic vs Tsunami",
+  "Under the Sea IN SPACE",
+  "The Man-Eating Shirt",
+  "The Demon of the Stall"]
+var descriptions=["When a series of mysterious murders occur in the little town of Nowheresville, Maine, the police are baffled. The only clue they have to do on is that all of the victims were killed by stab wounds to the ankles. As the town descends into chaos, the only hope left for the town is little Timmy and his psychic Labradoodle.",
+  "A scientist moves into town and locks himself in his basement. The town becomes more and more concerns as they start hearing strange noises underground - noises that sound an awful lot like an army of little footsteps...",
+  "A group of coding students are forced to go through a rigorous Javascript program and create fake websites, but what they don't know is that it is all part of a sinister scheme by their instructors... Also, there are snakes.",
+  "Captian Mirk and Mr. Spork are stuck on the HMS Enterprise with hundreds of space-snakes an must find a way to regain control of the ship before it gets caught in the gravity well of a nearby sun and incinerated",
+  "A tragic romance, in which a 1900s steamship and a tsunami are madly in love but can never be together because their families hate each other. Their plan to be together forever gets found out and they must make the hardest decision of their lives...",
+  "A group of brave astronauts go under the frozen waters of Jupiter's moon Europa. However, when their equipment begins to malfunction, they realize that Europa is actually an alien supercomputer, frozen to keep it from destroying the universe... and they just woke it up.",
+  "Tim Masterson can't believe his luck when he finds the greatest shirt ever for free at a garage sale. However, he soon learns that the shirt requires more than just dry-cleaning to look that good...",
+  "A heartwarming tale of friendship and teamwork as 4 buddies descend through the 9 levels of hell together through a portal in a creepy bathroom in an abandoned chinese restaurant, learning about each other and themselves throughout their journey"]
+var movieGenres=["Psycho Midgets",
+  "Psycho Midgets",
+  "Nightmare Snakes",
+  "Nightmare Snakes",
+  "Nautical",
+  "Nautical",
+  "Demons",
+  "Demons"]
+var images=['images/knife.jpeg',
+  'images/hobbits.jpg',
+  'images/code-fellows-logo.png',
+  'images/spaceGreen.jpg',
+  'images/wave.jpg',
+  'images/spacePlanet.jpeg',
+  'images/shirt.jpg',
+  'images/oblivion.jpeg']
+
 //initializes empty catalog
 var catalog = new Catalog()
 //Movie object
-function Movie(title, description, genre){
+function Movie(title, description, genre, img){
   this.title = title
   this.description = description
   this.genre = genre
+  this.img=img
 }
 //Catalog object
 function Catalog(){
@@ -24,7 +54,7 @@ function Catalog(){
 //function populates catalog
 function makeMovies(){
   for(i=0; i<titles.length;i++){
-    var m = new Movie(titles[i], descriptions[i], movieGenres[i])
+    var m = new Movie(titles[i], descriptions[i], movieGenres[i], images[i])
     catalog.contents.push(m)
   }
 }
@@ -42,7 +72,7 @@ function populateMovies(){
 function populateContent(){
   for(i=0; i<catalog.contents.length; i++){
     if (catalog.contents[i].title==movieBox.value){
-      contentBox.innerHTML = generator_blank(catalog.contents[i].title, catalog.contents[i].description)
+      contentBox.innerHTML = generator(catalog.contents[i].img, catalog.contents[i].title, catalog.contents[i].description)
     }
   }
 }
@@ -56,7 +86,10 @@ function populateSelect(element, list, type){
 }
 //generates a new content box with new title and content
 function generator_blank(title, content){
-  return '<div class="content__image"></div><div class="content__description-narrow"><h1>'+title+'</h1><p>'+content+'</p></div>'}
+  return '<div class="content__image"></div><div class="content__description-narrow"><h1 class=\'content__description__title\'>'+title+'</h1><p class=\'content__description__body\'>'+content+'</p></div>'}
+
+function generator(img, title, content){
+  return '<img src="'+img+'" class="content__image"/></div><div class="content__description-narrow"><h1 class=\'content__description__title\'>'+title+'</h1><p class=\'content__description__body\'>'+content+'</p></div>'}
 
 //initial functions, populate genre dropdown and populates catalog.contents with the movies
 populateSelect(genreBox, genreTitles, 'genre')
